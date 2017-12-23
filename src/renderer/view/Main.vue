51e87d73659cf0f3ead41c4745b6d54deae84f16
long.woo <longwunet@gmail.com>
2017-12-23 13:54:44 +0800

    修改main.vue

--- src/renderer/view/Main.vue
+++ src/renderer/view/Main.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    
+  <div class="jt-container">
+    <div class="jt-head"></div>
+    <div class="jt-body"></div>
+    <div class="jt-foot"></div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+<style scoped>
+.jt-container {
+  
+}
+</style>
