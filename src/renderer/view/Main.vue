75d12cf7d51855e221906c4a7c805c1b9d1ef99a
long.woo <longwunet@gmail.com>
2017-12-24 18:08:30 +0800

    update main.vue

--- src/renderer/view/Main.vue
+++ src/renderer/view/Main.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="jt-container">
-    <div class="jt-head"></div>
+    <div class="jt-head">
+      <div class="jt-head-time">{{playTime}}</div>
+      <div class="jt-head-name">{{playMusic}}</div>
+      <div class="jt-head-time">{{totalTime}}</div>
+    </div>
     <div class="jt-body"></div>
     <div class="jt-foot"></div>
   </div>
@@ -8,12 +12,28 @@
 
 <script>
 export default {
-  name: 'Main'
+  name: 'Main',
+  data () {
+    return {
+      playMusic: '',
+      playTime: '0:00',
+      totalTime: '0:00'
+    }
+  }
 }
 </script>
 
 <style scoped>
 .jt-container {
+  display: flex;
+  align-items: center;
+}
+
+.jt-head {
+  display: flex;
+}
+
+.jt-head-time {
   
 }
 </style>
